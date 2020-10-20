b33fcf31daf1cd33111743081b89a01cc1f41740
Cruizk <Cruiz.Kusman@siu.edu>
2020-10-20 15:32:49 -0500

    Changed project name to use underscore

--- techsite/codeeditor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodeeditorConfig(AppConfig):
-    name = 'codeeditor'
--- techsite/techsite/settings.py
+++ techsite/techsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'code_editor.apps.CodeEditorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
